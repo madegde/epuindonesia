71155ff1c857ec7591bd7d22f004eeca9a631cb3
madegde <madegde90@gmail.com>
2025-08-28 15:42:48 +0700

    update data and js

---
+++ js/events_data.js
@@ -0,0 +1,72 @@
+const majorEvents = [
+  {
+    "date": "1998-06-01",
+    "text": "Asian Financial Crisis",
+    "value": 319.96
+  },
+  {
+    "date": "2001-05-01",
+    "text": "Wahid Scandal<br>(Buloggate & Bruneigate)",
+    "value": 254.69
+  },
+  {
+    "date": "2002-08-01",
+    "text": "Constitutional<br>Amendments",
+    "value": 247.48
+  },
+  {
+    "date": "2003-12-01",
+    "text": "Iraq War & SARS<br>Epidemic",
+    "value": 200.58
+  },
+  {
+    "date": "2004-09-01",
+    "text": "Bomb Attacks &<br>Election Year",
+    "value": 170.79
+  },
+  {
+    "date": "2008-05-01",
+    "text": "Global Financial<br>Crisis",
+    "value": 128.56
+  },
+  {
+    "date": "2010-05-01",
+    "text": "Eurozone Debt<br>Crisis",
+    "value": 171.36
+  },
+  {
+    "date": "2011-12-01",
+    "text": "US Debt Ceiling<br>Crisis",
+    "value": 189.77
+  },
+  {
+    "date": "2014-12-01",
+    "text": "Fuel Subsidies<br>Reduction",
+    "value": 190.02
+  },
+  {
+    "date": "2015-10-01",
+    "text": "China Economic<br>Turmoil",
+    "value": 224.15
+  },
+  {
+    "date": "2016-11-01",
+    "text": "Trump Election",
+    "value": 182.5
+  },
+  {
+    "date": "2020-09-01",
+    "text": "COVID-19<br>Pandemic",
+    "value": 227.74
+  },
+  {
+    "date": "2022-08-01",
+    "text": "Russia-Ukraine<br>War",
+    "value": 220.29
+  },
+  {
+    "date": "2024-11-01",
+    "text": "US Election 2024",
+    "value": 186.57
+  }
+];
---
+++ js/stats_data.js
@@ -0,0 +1,30 @@
+const statsData = {
+  "total_articles": 6340,
+  "epu_articles": 6236,
+  "epu_percentage": 98.4,
+  "date_range": {
+    "start": "1994-10-04",
+    "end": "2024-12-31"
+  },
+  "publisher_stats": {
+    "The Jakarta Post": 4332,
+    "LKBN ANTARA": 1002,
+    "Bisnis Indonesia": 350,
+    "Tempo.co": 260,
+    "Tempo Magazine": 204,
+    "The Jakarta Globe": 192
+  },
+  "publisher_mapping": {
+    "JKPOST": "The Jakarta Post",
+    "BISNIS": "Bisnis Indonesia",
+    "ANTARA": "LKBN ANTARA",
+    "TEMPOE": "Tempo.co",
+    "JGLOBE": "The Jakarta Globe",
+    "TEMGEN": "Tempo Magazine"
+  },
+  "months_covered": 362,
+  "peak_epu": {
+    "date": "1998-06-01",
+    "value": 319.96
+  }
+};
